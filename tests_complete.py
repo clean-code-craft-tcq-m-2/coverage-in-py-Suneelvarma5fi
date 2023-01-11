261e4eb5ec7a19c17567e9ff754f6671024530e6
Suneel Varma <49975815+Suneelvarma5fi@users.noreply.github.com>
2023-01-11 11:08:36 +0530

    Avoided duplication of code

--- tests_complete.py
+++ tests_complete.py
@@ -2,33 +2,33 @@ import unittest
 from typewise_infer import ManageBattery
 
 class ManageBatteryTest(unittest.TestCase):
-  def test_classify_breachtype(self):
-    BatteryStatusTest1 = ManageBattery("TO_CONTROLLER","HI_ACTIVE_COOLING",32)
-    self.assertTrue(BatteryStatusTest1.classify_and_infer_and_alert())
+  def test_classify_managebattery(self):
+    BatteryStatusTest10 = ManageBattery("TO_CONTROLLER","HI_ACTIVE_COOLING",32)
+    self.assertTrue(BatteryStatusTest10.classify_and_infer_and_alert())
 
-    BatteryStatusTest2 = ManageBattery("TO_EMAIL","HI_ACTIVE_COOLING",46)
-    self.assertTrue(BatteryStatusTest2.classify_and_infer_and_alert())
+    BatteryStatusTest11 = ManageBattery("TO_EMAIL","HI_ACTIVE_COOLING",46)
+    self.assertTrue(BatteryStatusTest11.classify_and_infer_and_alert())
 
-    BatteryStatusTest3 = ManageBattery("TO_CONTROLLER","HI_ACTIVE_COOLING",-32)
-    self.assertTrue(BatteryStatusTest3.classify_and_infer_and_alert())
+    BatteryStatusTest12 = ManageBattery("TO_CONTROLLER","HI_ACTIVE_COOLING",-32)
+    self.assertTrue(BatteryStatusTest12.classify_and_infer_and_alert())
 
-    BatteryStatusTest4 = ManageBattery("TO_EMAIL","PASSIVE_COOLING",32)
-    self.assertTrue(BatteryStatusTest4.classify_and_infer_and_alert())
+    BatteryStatusTest13 = ManageBattery("TO_EMAIL","PASSIVE_COOLING",32)
+    self.assertTrue(BatteryStatusTest13.classify_and_infer_and_alert())
 
-    BatteryStatusTest5 = ManageBattery("TO_CONTROLLER","PASSIVE_COOLING",46)
-    self.assertTrue(BatteryStatusTest5.classify_and_infer_and_alert())
+    BatteryStatusTest14 = ManageBattery("TO_CONTROLLER","PASSIVE_COOLING",46)
+    self.assertTrue(BatteryStatusTest14.classify_and_infer_and_alert())
 
-    BatteryStatusTest6 = ManageBattery("TO_EMAIL","PASSIVE_COOLING",-32)
-    self.assertTrue(BatteryStatusTest6.classify_and_infer_and_alert())
+    BatteryStatusTest15 = ManageBattery("TO_EMAIL","PASSIVE_COOLING",-32)
+    self.assertTrue(BatteryStatusTest15.classify_and_infer_and_alert())
 
-    BatteryStatusTest7 = ManageBattery("TO_CONTROLLER","MED_ACTIVE_COOLING",32)
-    self.assertTrue(BatteryStatusTest7.classify_and_infer_and_alert())
+    BatteryStatusTest16 = ManageBattery("TO_CONTROLLER","MED_ACTIVE_COOLING",32)
+    self.assertTrue(BatteryStatusTest16.classify_and_infer_and_alert())
 
-    BatteryStatusTest8 = ManageBattery("TO_CONTROLLER","MED_ACTIVE_COOLING",46)
-    self.assertTrue(BatteryStatusTest8.classify_and_infer_and_alert())
+    BatteryStatusTest17 = ManageBattery("TO_CONTROLLER","MED_ACTIVE_COOLING",46)
+    self.assertTrue(BatteryStatusTest17.classify_and_infer_and_alert())
     
-    BatteryStatusTest9 = ManageBattery("TO_EMAIL","MED_ACTIVE_COOLING",-32)
-    self.assertTrue(BatteryStatusTest9.classify_and_infer_and_alert())
+    BatteryStatusTest18 = ManageBattery("TO_EMAIL","MED_ACTIVE_COOLING",-32)
+    self.assertTrue(BatteryStatusTest18.classify_and_infer_and_alert())
 
 if __name__ == '__main__':
   unittest.main()
